945509f76e3f2a17e00059e77ad1649d35d20dfc
lkm1developer <lkm1developer@gmail.com>
2022-12-06 17:46:25 +0530

    cleanup

--- src/sections/@dashboard/team/team-details/index.js
+++
@@ -1,5 +0,0 @@
-export { default as TallyDetailsReview } from './TeamDetailsReview';
-export { default as TallyDetailsSummary } from './TeamDetailsSummary';
-export { default as TallyDetailsCarousel } from './TeamDetailsCarousel';
-export { default as TallyDetailsReview } from './TeamDetailsReview';
-export { default as TallyDetailsReviewOverview } from './TeamDetailsReviewOverview';
--- src/sections/@dashboard/team/team-list/index.js
+++
@@ -1,2 +0,0 @@
-export { default as TeamTableRow } from './teamTableRow';
-export { default as TeamTableToolbar } from './teamTableToolbar';
